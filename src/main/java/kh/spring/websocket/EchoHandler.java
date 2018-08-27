4659d9fe2341c77400946c88053c37a54ce8156d
seop <gudrhks12992@naver.com>
2018-08-27 11:13:27 +0900

    d

--- src/main/java/kh/spring/websocket/EchoHandler.java
+++ src/main/java/kh/spring/websocket/EchoHandler.java
@@ -1,47 +1,58 @@
 package kh.spring.websocket;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
-import org.mybatis.spring.SqlSessionTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Repository;
-import org.springframework.web.socket.CloseStatus;
-import org.springframework.web.socket.TextMessage;
-import org.springframework.web.socket.WebSocketSession;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.handler.TextWebSocketHandler;
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
 
-@Configuration
-@Repository
-@EnableWebSocket
-public class EchoHandler extends TextWebSocketHandler {
-	@Autowired
-	private SqlSessionTemplate template;
+import org.springframework.stereotype.Controller;
+
+@ServerEndpoint("/websocket")
+@Controller
+public class EchoHandler{
 	
-	private Set<WebSocketSession> sessionList = new HashSet<>();
+	private  Set<Session> users = Collections.synchronizedSet(new HashSet<>());
 	
-	// 클라이언트와 연결 된 후
-	public void afterConnectionEstablished(WebSocketSession session) {
-		sessionList.add(session);
-		System.out.println("들어왔습니다" + session);
-		//System.out.println("채팅방 입장: " + session.getPrincipal().getName());
+	
+	@OnOpen
+	public void hadleOpen(Session session) {
+		users.add(session);
+		System.out.println(users.size());
 	}
 	
-	// 클라이언트가 웹소켓서버로 메시지를 전송했을 때 실행되는 메소드
-	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-		// 연결된 모든 클라이언트에게 메세지 전송
-		System.out.println(message);
-		System.out.println(session);
-		for(WebSocketSession sess : sessionList) {
-			sess.sendMessage(new TextMessage(session.getPrincipal().getName() + "|" + message.getPayload()));
+	@OnMessage
+	public void handleMessage(String message,Session session) {
+		try {
+			for(Session tmp : users) {
+				System.out.println(message);
+				tmp.getBasicRemote().sendText(message);
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
 		}
+		
+		
+	
+		
 	}
 	
-	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
-		sessionList.remove(session);
-		// System.out.println("채팅방 퇴장: " + session.getPrincipal().getName());
+	@OnClose
+	public void handleClose(Session session) {
+		users.remove(session);
 	}
-
+	
+	@OnError
+	public void handleError(Throwable t) {
+		t.printStackTrace();
+	}
 }
+
+
+
